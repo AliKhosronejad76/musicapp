954e76d42ad97df13fcbb5c0da0a992e33e5b96a
Ali Khosronejad <khosronejadali344@gmail.com>
2024-07-31 18:36:12 +0330

    route.jsx created

---
+++ src/components/router/index.jsx
@@ -0,0 +1,18 @@
+import { Routes , Route } from "react-router-dom"
+
+function Router(){
+    return(
+        <Routes>
+            <Route path="/" element={}/>
+            {/* <Route path="" element={}/>
+            <Route path="" element={}/>
+            <Route path="" element={}/>
+            <Route path="" element={}/>
+            <Route path="" element={}/>
+            <Route path="" element={}/> */}
+
+        </Routes>
+    )
+}
+
+export default Router

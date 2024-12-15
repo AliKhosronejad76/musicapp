6e5eed7252ee003740d77e80ffd658399a4d0cf1
AliKhosronejad76 <121498183+AliKhosronejad76@users.noreply.github.com>
2024-12-15 20:19:03 +0330

    Update index.jsx

--- src/pages/single/index.jsx
+++ src/pages/single/index.jsx
@@ -1,11 +1,13 @@
+import { useParams } from "react-router-dom"
 import MusicPost from "./MusicPost"
 import List from "./List"
 import Comments from "./Comments" 
 
 function Single(){
+    const {id} = useParams()
     return(
         <div className="flex flex-col md:flex-row justify-between">
-            
+            <h1 className="text-7xl"> {id}</h1>
             <div className="w-full md:w-[70%]">
                 <MusicPost/>
                 {/* more post box */}

d09b554c0bebc59ff0661874622e13e2d2a0643e
AliKhosronejad76 <121498183+AliKhosronejad76@users.noreply.github.com>
2024-12-15 20:33:19 +0330

    Update index.jsx

--- src/pages/single/index.jsx
+++ src/pages/single/index.jsx
@@ -1,10 +1,22 @@
+import { useState  , useEffect } from "react"
 import { useParams } from "react-router-dom"
 import MusicPost from "./MusicPost"
 import List from "./List"
 import Comments from "./Comments" 
+import {playList} from "../../data.js"
+
 
 function Single(){
     const {id} = useParams()
+    const [musicData , setMusicData] = useState([]);
+    useEffect(()=>{
+        const setMusic = ()=>{
+            const music = playList.filter(music=> music.id == id)
+            setMusicData(music)
+        }
+        setMusic()
+        console.log(musicData)
+    },[id])
     return(
         <div className="flex flex-col md:flex-row justify-between">
             <h1 className="text-7xl"> {id}</h1>

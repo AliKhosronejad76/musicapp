652d2ea3abd06dc68020e29d52abc8978c5ce705
AliKhosronejad76 <121498183+AliKhosronejad76@users.noreply.github.com>
2024-12-15 13:32:18 +0330

    Update index.jsx

--- src/pages/movie/index.jsx
+++ src/pages/movie/index.jsx
@@ -1,19 +1,12 @@
 import PostItem from "../../components/postitem"
 import Pageination  from "../../components/pageination"
+import {playList} from "../../data.js"
 function Movies(){
     return(
         <div className="w-full">
             <h1 className="text-bold text-xl mt-4"> فیلم ها</h1>
           <div>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
+             {playList.map((item , index )=><PostItem key={index} post={item} />)}
 
           </div>
          <Pageination/>

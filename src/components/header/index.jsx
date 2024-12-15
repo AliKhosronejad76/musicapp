0818c07f73b8ee281c2c96f842d02f72dca8b556
AliKhosronejad76 <121498183+AliKhosronejad76@users.noreply.github.com>
2024-12-15 21:43:38 +0330

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from  "react"
 import Navbar from "./navbar"
 import Banner from "./banner"
+import {useLocation} from "react-router-dom"
+
+
 function Header(){
+  const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
+   useEffect(() => {
+     window.scrollTo(0, 0);
+   }, [pathname]);
     return(
         <>
             <div className="sticky z-[100]  top-0 transition duration-700">

ecdd83ff8b2a95c5dd84c46208f1c175df384cff
AliKhosronejad76 <121498183+AliKhosronejad76@users.noreply.github.com>
2024-12-15 13:17:03 +0330

    Update index.jsx

--- src/pages/albums/index.jsx
+++ src/pages/albums/index.jsx
@@ -1,19 +1,13 @@
 import { useLocation } from 'react-router-dom'
 import PostItem from "../../components/postitem"
-
+import {playList} from "../../data.js"
 
 function Albums(){
     
     return(
         <div className="w-full">
             <h1 className="text-bold text-xl mt-4" >آلبوم ها </h1>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-            <PostItem/>
-
+             {playList.map((item , index )=><PostItem key={index} post={item} />)}
         </div>
     )
 }
